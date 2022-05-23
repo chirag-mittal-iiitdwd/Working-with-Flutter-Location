8f9e0a608cf4eefa85795958f43b37a13012c9f5
Chirag Mittal <chirag.workid@gmail.com>
2022-05-23 23:06:38 +0530

    initial

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,69 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class LocationInput extends StatefulWidget {
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String _previewImage;
+
+  Future<void> _getCurrentUserLocation() async {
+    final locData = await Location().getLocation();
+    print(locData.latitude);
+    print(locData.longitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.center,
+          height: 170,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Colors.grey,
+            ),
+          ),
+          child: _previewImage == null
+              ? Text(
+                  'No Location Chosen',
+                  textAlign: TextAlign.center,
+                )
+              : Image.network(
+                  _previewImage,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlatButton.icon(
+              onPressed: _getCurrentUserLocation,
+              icon: Icon(
+                Icons.location_on,
+              ),
+              label: Text('Current Location'),
+              textColor: Theme.of(context).primaryColor,
+            ),
+            FlatButton.icon(
+              onPressed: () {},
+              icon: Icon(
+                Icons.map,
+              ),
+              label: Text('Select on Map'),
+              textColor: Theme.of(context).primaryColor,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
